4b64a74c6d91506313913587fb12f3100a94f891
alvaro <alvaro.f.pereyra@gmail.com>
2021-02-25 19:27:29 +0000

    changed hello-world

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,16 @@
 """
 a program
 """
-print("Hello World")
+
+
+def main():
+    print("Hello World")
+    print(f"Filename is {__file__}. Name is {__name__}.\n Docstring is {__doc__}.")
+
+
+def some_new_function():
+    pass
+
+
+if __name__ == "__main__":
+    main()
